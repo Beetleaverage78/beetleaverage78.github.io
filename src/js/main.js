c2f00ff640c7f2558e5a0dc201813581498aeaef
PlacidBeetle <Beetleaverage78@users.noreply.github.com>
2020-12-23 15:56:22 +1300

    Responsive Testing

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,13 @@
-new fullpage('#fullPage', {
-  autoScrolling: true,
-  anchors: ['section1', 'section2']
-})
+$(document).ready(function () {
+  new fullpage('#fullPage', {
+    autoScrolling: true,
+    anchors: ['section1', 'section2', 'section3'],
+    setMouseWheelScrolling: false,
+  });
+
+  $(".slider").slick({
+    arrows: true,
+  });
+});
+
+
